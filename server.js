77ad7787283815aca774bc163ea1149b72877447
ktp1451 <76793752+ktp1451@users.noreply.github.com>
2021-05-01 09:58:39 -0500

    updated code

--- server.js
+++ server.js
@@ -18,16 +18,17 @@ const runSearch = () => {
       })
       
       .then(function (answer) {
-        if (answer.action === "VIEW") {
+        console.log(answer);
+        if (answer.action === "View All Employees") {
             view.viewData();
         }
-        else if (answer.action === "ADD") {
-            add.addData();
+        else if (answer.action === "Add Employee") {
+            add.addEmployee();
         }
-        else if (answer.action === "DELETE") {
+        else if (answer.action === "Remove Employee") {
             deletD.deleteData();
         }
-        else if (answer.action === "UPDATE") {
+        else if (answer.action === "Update Employee Role") {
             up.updateData();
         }
         else {
@@ -35,6 +36,7 @@ const runSearch = () => {
             connection.end();
         }
     });
+    
 }
 
 connection.connect(function(err){
@@ -42,107 +44,56 @@ connection.connect(function(err){
         runSearch()
 })
 
-// function addRole() {
-//     connection.query(`SELECT * FROM department`, function (err, res) {
-//         if (err) throw err;
-//         console.table(res);
-//         inquirer
-//             .prompt([{
-//                 name: "addDepart",
-//                 type: "list",
-//                 choices: function () {
-//                     var departArray = [];
-//                     for (var i = 0; i < res.length; i++) {
-//                         departArray.push(res[i].id);
-//                     }
-//                     return departArray;
-//                 },
-//                 message: "What department does this role belong to?",
-//             },
-//             {
-//                 name: "addRole",
-//                 type: "input",
-//                 message: "What name do you want for this role?",
-//             },
-//             {
-//                 name: "addSalary",
-//                 type: "input",
-//                 message: "What is the salary?",
-//                 validate: function (value) {
-//                     if (isNaN(value) === false) {
-//                         return true;
-//                     }
-//                     return false;
-//                 }
-//             },
-//             ]).then(function (answer) {
-//                 connection.query(
-//                     "INSERT INTO role SET ?",
-//                     {
-//                         role_title: answer.addRole,
-//                         salary: answer.addSalary,
-//                         department_id: answer.addDepart
-//                     },
-//                     function (err, results) {
-//                         if (err) throw err;
-//                         console.log("New Role has been added!");
-//                         addData();
-//                     }
-//                 );
-//             })
-//     })
-// }
-// function addEmployee() {
-//     connection.query(
-//         `SELECT e.first_name, e.last_name, e.id AS employee_id, r.id AS role_id, r.role_title, d.department_name
-//         FROM employee e
-//         LEFT JOIN employee em ON e.manager_id = em.id
-//         INNER JOIN role r ON e.role_id = r.id
-//         INNER JOIN department d ON r.department_id = d.id
-//         ORDER BY e.id`,
-//         function (err, res) {
-//             console.table(res);
-//             inquirer
-//                 .prompt([{
-//                     name: "addEmployeeFirstName",
-//                     type: "input",
-//                     message: "What the first name of the employee?",
-//                 },
-//                 {
-//                     name: "addEmployeeLastName",
-//                     type: "input",
-//                     message: "What's the last name of the employee?",
-//                 },
-//                 {
-//                     name: "addEmployeeRole",
-//                     type: "list",
-//                     message: "What is the employee's role?",
-//                     choices: function () {
-//                         var roleArray = [];
-//                         for (var i = 0; i < res.length; i++) {
-//                             roleArray.push(res[i].role_id);
-//                         }
-//                         let removeDups = new Set(roleArray)
-//                         let newRoleArr = [...removeDups]; 
-//                         return newRoleArr;
-//                     },
-//                 },
-//                 ]).then(function (answer) {
-//                     connection.query(
-//                         "INSERT INTO employee SET ?",
-//                         {
-//                             first_name: answer.addEmployeeFirstName,
-//                             last_name: answer.addEmployeeLastName,
-//                             role_id: answer.addEmployeeRole,
-//                         },
-//                         function (err, results) {
-//                             if (err) throw err;
-//                             console.log("New employee has been added!");
-//                             addData();
-//                         }
-//                     );
-//                 })
-//         }
-//     )
-// }
-// module.exports = { addData }
+function addEmployee () {
+    connection.query(
+        `SELECT e.first_name, e.last_name, e.id AS employee_id, r.id AS role_id, r.role_title, d.department_name
+        FROM employee e
+        LEFT JOIN employee em ON e.manager_id = em.id
+        INNER JOIN role r ON e.role_id = r.id
+        INNER JOIN department d ON r.department_id = d.id
+        ORDER BY e.id`,
+        function (err, res) {
+            console.table(res);
+            inquirer
+                .prompt([{
+                    name: "addEmployeeFirstName",
+                    type: "input",
+                    message: "What the first name of the employee?",
+                },
+                {
+                    name: "addEmployeeLastName",
+                    type: "input",
+                    message: "What's the last name of the employee?",
+                },
+                {
+                    name: "addEmployeeRole",
+                    type: "list",
+                    message: "What is the employee's role?",
+                    choices: function () {
+                        var roleArray = [];
+                        for (var i = 0; i < res.length; i++) {
+                            roleArray.push(res[i].role_id);
+                        }
+                        let removeDups = new Set(roleArray)
+                        let newRoleArr = [...removeDups]; 
+                        return newRoleArr;
+                    },
+                },
+                ]).then(function (answer) {
+                    connection.query(
+                        "INSERT INTO employee SET ?",
+                        {
+                            first_name: answer.addEmployeeFirstName,
+                            last_name: answer.addEmployeeLastName,
+                            role_id: answer.addEmployeeRole,
+                        },
+                        function (err, results) {
+                            if (err) throw err;
+                            console.log("New employee has been added!");
+                            addEmployee();
+                        }
+                    );
+                })
+        }
+    )
+}
